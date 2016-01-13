5286da172b4fd2b047bc45e2025f535c2406f4b6
sogdahl <steve.ogdahl@gmail.com>
2016-01-12 18:53:55 -0600

    Create sample_1.py

---
+++ flows/sample_1.py
@@ -0,0 +1,43 @@
+__author__ = 'Steven Ogdahl'
+
+from utility.decaptcha import processing
+
+steps = [
+    ['force_foreground'],
+    ['remove_alpha', {
+        'background': processing.WHITE
+    }],
+    ['threshold', {
+        'threshold': 0.75
+    }],
+    ['remove_grid', {
+        'keep_intersections': True
+    }],
+    ['stitch_orthogonal_gaps', {
+        'color': processing.BLACK
+    }],
+    ['remove_lonely', {
+        'max_neighbors': 1
+    }],
+    ['stitch_kissing_corners', {
+        'color': processing.BLACK
+    }],
+    ['remove_juts', {
+        'color': processing.BLACK
+    }],
+    ['stitch_orthogonal_gaps', {
+        'color': processing.BLACK
+    }],
+    ['center_data'],
+    ['fill_shapes', {
+        'fill_holes': False,
+        'separation': 4,
+        'edge_depth': 2,
+        'minimum_size': 15
+    }],
+    ['threshold', {
+        'threshold': 0.75
+    }]
+]
+
+charset = 'ABCDEF0123456789'
