2ac64786a08d48e8385332e013533234640d2757
sogdahl <steve.ogdahl@gmail.com>
2016-01-12 18:51:47 -0600

    Create errors.py

---
+++ errors.py
@@ -0,0 +1,20 @@
+__author__ = 'Steven Ogdahl'
+"""Test for exceptions raised in the tesseract logfile"""
+
+class Tesser_General_Exception(Exception):
+    pass
+
+class Tesser_Invalid_Filetype(Tesser_General_Exception):
+    pass
+
+def check_for_errors(logfile = "tesseract.log"):
+    text = ""
+    try:
+        inf = file(logfile)
+        text = inf.read()
+        inf.close()
+    except Exception:
+        pass
+    # All error conditions result in "Error" somewhere in logfile
+    if text.find("Error") != -1:
+        raise Tesser_General_Exception, text
