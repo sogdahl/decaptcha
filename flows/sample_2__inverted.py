103697defca35ca3fa264a02193e736b62238b15
sogdahl <steve.ogdahl@gmail.com>
2016-01-12 18:55:04 -0600

    Create sample_2__inverted.py

---
+++ flows/sample_2__inverted.py
@@ -0,0 +1,41 @@
+__author__ = 'Steven Ogdahl'
+
+from utility.decaptcha import processing
+
+steps = [
+    ['invert'],
+    ['separate_regions', {
+        'regions': ((76, 0, 109, 50), (109, 0, 142, 50), (142, 0, 175, 50)),
+        'separation': 5,
+        'background': processing.WHITE
+    }],
+    ['remove_bbox', {
+        'border': (70, 4, 70, 3),
+        'background': processing.WHITE
+    }],
+    ['quantize', {
+        'colors': 2,
+        'palette': processing.PALETTE_IMAGE
+    }],
+    ['remove_lonely', {
+        'max_neighbors': 4,
+        'color': processing.BLACK
+    }],
+    ['remove_lonely', {
+        'max_neighbors': 4,
+        'color': processing.BLACK
+    }],
+    ['remove_lonely', {
+        'max_neighbors': 4,
+        'color': processing.BLACK
+    }],
+    ['remove_lonely', {
+        'max_neighbors': 4,
+        'color': processing.BLACK
+    }],
+    ['remove_juts', {
+        'color': processing.BLACK
+    }]
+]
+
+charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
