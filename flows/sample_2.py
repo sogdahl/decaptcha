c0a3d3fc35fb49db51361870e07b7cfd65582681
sogdahl <steve.ogdahl@gmail.com>
2016-01-12 18:54:28 -0600

    Create sample_2.py

---
+++ flows/sample_2.py
@@ -0,0 +1,20 @@
+__author__ = 'Steven Ogdahl'
+
+steps = [
+    ['decide_flow', {
+        'flows': {
+            'sample_2__grid': {
+                'histogram': {
+                    'black': (0.27, 0.37),
+                    'white': (0.59, 0.69)
+                }
+            },
+            'sample_2__inverted': {
+                'histogram': {
+                    'black': (0.49, 0.60),
+                    'white': (0.23, 0.34)
+                }
+            }
+        }
+    }]
+]
